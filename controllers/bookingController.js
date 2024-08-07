5ca8dab1e640fc1157231e602f963d1230c7e8f5
Accia <stefanocammarata@outlook.it>
2024-08-07 14:33:15 +0200

    Updated bookingController again

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -8,48 +8,46 @@ const factory = require('./handlerFactory');
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 1) Get the currently booked tour
   const tour = await Tour.findById(req.params.tourId);
-  // console.log(tour);
 
   // 2) Create checkout session
   const session = await stripe.checkout.sessions.create({
+    /*
+      SESSION INFO
+    */
     payment_method_types: ['card'],
-    // success_url: `${req.protocol}://${req.get('host')}/my-tours/?tour=${
-    //   req.params.tourId
-    // }&user=${req.user.id}&price=${tour.price}`,
+    // success_url: `${req.protocol}://${req.get('host')}/my-tours/?tour=${req.params.tourId}&user=${req.user.id}&price=${tour.price}`, // NOTE : this by any mean is not a secure way to pass the data, by knowing the endpoint the query could be forged without the checkout process
     success_url: `${req.protocol}://${req.get('host')}/my-tours?alert=booking`,
+    mode: 'payment',
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
-    price_data: [
+    /*
+      PRODUCT INFO
+    */
+    line_items: [
       {
-        name: `${tour.name} Tour`,
-        description: tour.summary,
-        images: [
-          `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`,
-        ],
-        amount: tour.price * 100,
-        currency: 'usd',
         quantity: 1,
+        price_data: {
+          unit_amount: tour.price * 100,
+          currency: 'eur', // 'usd' for Dollar
+          product_data: {
+            name: `${tour.name} Tour`,
+            images: [
+              `https://natours-black-ten.vercel.app/img/tours/${tour.imageCover}`,
+            ],
+            description: tour.summary,
+          },
+        },
       },
     ],
   });
 
-  // 3) Create session as response
+  // 3) Send it to the client
   res.status(200).json({
     status: 'success',
     session,
   });
 });
-
-// exports.createBookingCheckout = catchAsync(async (req, res, next) => {
-//   // This is only TEMPORARY, because it's UNSECURE: everyone can make bookings without paying
-//   const { tour, user, price } = req.query;
-
-//   if (!tour && !user && !price) return next();
-//   await Booking.create({ tour, user, price });
-
-//   res.redirect(req.originalUrl.split('?')[0]);
-// });
 
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
