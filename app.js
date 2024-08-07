386b607b7d97a04e3243840412964499c90cdda9
Accia <stefanocammarata@outlook.it>
2024-08-07 11:02:51 +0200

    Implemented CORS

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const csp = require('express-csp');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -26,6 +27,11 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1- MIDDLEWARES
+// Implement CORS
+app.use(cors());
+
+app.options('*', cors());
+
 // serving static file
 app.use(express.static(path.join(__dirname, 'public')));
 
